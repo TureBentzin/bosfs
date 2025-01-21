5736e2ed77fc981079c3bbce4d01fa2c9f7693bc
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-01-21 12:54:09 +0100

    add cli

---
+++ bosfs-cli/bosfs.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Tureb on 21.01.2025.
+//
+
+#include "bosfs.h"
+
+
+int main(int argc, char** argv) {
+
+}
---
+++ bosfs-cli/bosfs.h
@@ -0,0 +1,8 @@
+//
+// Created by Tureb on 21.01.2025.
+//
+
+#ifndef BOSFS_BOSFS_H
+#define BOSFS_BOSFS_H
+
+#endif //BOSFS_BOSFS_H
