4aca5e9d39173be9f13760f243af9520e752dd88
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-01-21 13:15:00 +0100

    moved cli to dynamic dependency

--- bosfs-cli/bosfs.cpp
+++ bosfs-cli/bosfs.cpp
@@ -4,7 +4,32 @@
 
 #include "bosfs.h"
 
+#include "cli/loopscheduler.h"
+#include "cli/clilocalsession.h"
 
-int main(int argc, char** argv) {
+
+int main(int argc, char **argv) {
+
+    auto root = std::make_unique<cli::Menu>("bosfs");
+
+    root->Insert("test",
+                 [](std::ostream &out) { out << "Test command executed!" << std::endl; },
+                 "Test the cli lib");
+
+    cli::Cli cli(std::move(root)); //move root for performance
+
+    cli.ExitAction([](std::ostream &out) {
+        out << "BOSFS by Ture Bentzin" << std::endl;
+    });
+
+
+    cli::LoopScheduler scheduler;
+    cli::CliLocalTerminalSession session(cli, scheduler, std::cout);
+
+    session.ExitAction([&scheduler](std::ostream &out) {
+        scheduler.Stop();
+    });
+
+    scheduler.Run();
 
 }
